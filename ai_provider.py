b1d5cacf782f852da8d4b34f5d8437dd04578ad9
nikita <easygames420@bk.ru>
2025-02-25 15:59:56 +0500

    adding

--- ai_provider.py
+++ ai_provider.py
@@ -1,8 +1,23 @@
 from g4f.client import Client
 
+import logging
+from logging.handlers import RotatingFileHandler
+
+# logger config
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+# Setup logger
+logger = logging.getLogger(__name__)
+logger.addHandler(RotatingFileHandler(filename=f"logs/{__name__}.log",
+                                      mode='w',
+                                      maxBytes=1024 * 1024))
+
 
 class AI:
-    def __init__(self, model:str = 'free'):
+    def __init__(self, model: str = 'free'):
         if model == 'free':
             self.client = Client()
             self.model = "gpt-4o-mini"
@@ -11,7 +26,7 @@ class AI:
         elif model == 'deepseek':
             ...
 
-    def translate(self, text:str = '', lang:str = 'ru'):
+    def translate(self, text: str = '', lang: str = 'ru'):
         """
         Translates data through LLM
 
@@ -23,9 +38,9 @@ class AI:
             content = f"Please translate the following text to {lang}. Text: {text}"
             response = self.client.chat.completions.create(
                 messages=[{
-                            "role": "user",
-                            "content": content
-                        }],
+                    "role": "user",
+                    "content": content
+                }],
                 model=self.model
             )
 
@@ -33,7 +48,7 @@ class AI:
         except Exception as e:
             print(f"Failed to translate via LLM: {e}")
 
-    def decompose(self, task: str = '', lang:str = 'ru'):
+    def decompose(self, task: str = '', lang: str = 'ru'):
         """
         Decomposes task through LLM
 
@@ -54,8 +69,55 @@ class AI:
         except Exception as e:
             print(f'Failed to decompose via LLM {e}')
 
+    def extract_pd(self, task: str = '', lang: str = 'ru'):
+        """
+        Extract personal data from task through LLM
+
+        Args:
+            task (str): Task for decompose
+            lang (str): Language code
+        """
+        try:
+            content = (f'Please extract data like personal data, e-mail, phone, age etc from the following task.'
+                       f'Task: {task}')
+            response = self.client.chat.completions.create(
+                messages=[{
+                    "role": "user",
+                    "content": content
+                }],
+                model=self.model
+            )
+            return response.choices[0].message.content
+        except Exception as e:
+            print(f'Failed to decompose via LLM {e}')
+
+    def generate(self, decomposed_task: str = '', url: str = '', lang: str = 'ru', ):
+        """
+        Generates code with selenium for decomposed task on url through LLM
+
+        Args:
+            decomposed_task (str): Decomposed task for completes
+            url (str): Url
+            lang (str): Language code
+        """
+
+        try:
+            content = (f'Please generate code for complete the following task on {url} with python framework selenium.'
+                       f'Task: {decomposed_task}')
+            response = self.client.chat.completions.create(
+                messages=[{
+                    "role": "user",
+                    "content": content
+                }],
+                model=self.model
+            )
+            return response.choices[0].message.content
+        except Exception as e:
+            print(f'Failed to generate code via LLM {e}')
 
 
 ai = AI()
-# print(ai.translate(text='fuck off boy', lang='ru'))
-print(ai.decompose(task='Необходимо написать сайт на fast api чтобы выполнить заказ для интернет магазина', lang='ru'))
+
+if __name__ == '__main__':
+    data = ai.extract_pd(task='Необходимо заполнить поля следующим e mail: sobaka@bk.ru ')
+    print(data)
